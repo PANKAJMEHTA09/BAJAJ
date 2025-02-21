9501394cdfc8a1c1ad1133955d39c774fb79e465
PANKAJMEHTA09 <pm955893@gmail.com>
2025-02-21 17:03:57 +0530

    second commit

--- Qualifier1/src/main/java/com/example/Qualifier1/Controller/ApiController.java
+++ Qualifier1/src/main/java/com/example/Qualifier1/Controller/ApiController.java
@@ -1,4 +1,4 @@
-package com.example.Qualifier1.ApiController;
+package com.example.Qualifier1.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
